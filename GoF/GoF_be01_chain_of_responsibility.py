6b025dd249cca7e86efcf9e9d32856df1359d4d7
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-05-16 05:03:57 +0900

    GoF chain of responsibility

---
+++ GoF/GoF_be01_chain_of_responsibility.py
@@ -0,0 +1,27 @@
+class Handler:
+    def __init__(self, successor=None):
+        self._successor = successor
+
+    def handle_request(self):
+        pass
+
+
+class ConcreteHandler1(Handler):
+    def handle_request(self):
+        print("ConcreteHandler1: Handling request.")
+        if self._successor:
+            self._successor.handle_request()
+
+
+class ConcreteHandler2(Handler):
+    def handle_request(self):
+        print("ConcreteHandler2: Handling request.")
+        if self._successor:
+            self._successor.handle_request()
+
+
+if __name__ == "__main__":
+    handler1 = ConcreteHandler1()
+    handler2 = ConcreteHandler2(handler1)
+
+    handler2.handle_request()
