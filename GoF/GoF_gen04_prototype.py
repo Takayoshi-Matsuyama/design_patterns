72b4a1580075b0171c6af325707d008bbede3d5e
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-05-22 05:50:27 +0900

    GoF - Prototype

---
+++ GoF/GoF_gen04_prototype.py
@@ -0,0 +1,23 @@
+class Prototype:
+    def clone(self):
+        raise NotImplementedError("You should implement the clone method.")
+
+
+class ConcretePrototype1(Prototype):
+    def clone(self):
+        return ConcretePrototype1()
+
+
+class ConcretePrototype2(Prototype):
+    def clone(self):
+        return ConcretePrototype2()
+
+
+if __name__ == "__main__":
+    prototype1 = ConcretePrototype1()
+    clone1 = prototype1.clone()
+    print(f"Cloned object of type: {type(clone1).__name__}")
+
+    prototype2 = ConcretePrototype2()
+    clone2 = prototype2.clone()
+    print(f"Cloned object of type: {type(clone2).__name__}")
