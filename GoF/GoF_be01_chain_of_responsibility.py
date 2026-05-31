697e3ff7b7548218b5212cf93eacef13731687dd
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-06-01 05:53:44 +0900

    GoF - Chain of Responsibility

--- GoF/GoF_be01_chain_of_responsibility.py
+++ GoF/GoF_be01_chain_of_responsibility.py
@@ -1,12 +1,15 @@
 class Handler:
     def __init__(self, successor=None):
-        self._successor = successor
+        raise NotImplementedError("You should implement this method in subclasses")
 
     def handle_request(self):
-        pass
+        raise NotImplementedError("You should implement this method in subclasses")
 
 
 class ConcreteHandler1(Handler):
+    def __init__(self, successor=None):
+        self._successor = successor
+
     def handle_request(self):
         print("ConcreteHandler1: Handling request.")
         if self._successor:
@@ -14,6 +17,9 @@ class ConcreteHandler1(Handler):
 
 
 class ConcreteHandler2(Handler):
+    def __init__(self, successor=None):
+        self._successor = successor
+
     def handle_request(self):
         print("ConcreteHandler2: Handling request.")
         if self._successor:
