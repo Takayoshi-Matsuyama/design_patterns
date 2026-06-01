1756482af7969939b9c5c4757909d6b7c6cc6895
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-06-02 07:20:49 +0900

    GoF - AbstractFactory

--- GoF/GoF_gen01_abstract_factory.py
+++ GoF/GoF_gen01_abstract_factory.py
@@ -1,42 +1,66 @@
 class AbstractProductA:
+    def __init__(self):
+        raise NotImplementedError("This method should be implemented by subclasses")
+
     def operation_a(self):
-        pass
+        raise NotImplementedError("This method should be implemented by subclasses")
 
 
 class ProductA1(AbstractProductA):
+    def __init__(self):
+        pass
+
     def operation_a(self):
         return "ProductA1 operation"
 
 
 class ProductA2(AbstractProductA):
+    def __init__(self):
+        pass
+
     def operation_a(self):
         return "ProductA2 operation"
 
 
 class AbstractProductB:
+    def __init__(self):
+        raise NotImplementedError("This method should be implemented by subclasses")
+
     def operation_b(self):
-        pass
+        raise NotImplementedError("This method should be implemented by subclasses")
 
 
 class ProductB1(AbstractProductB):
+    def __init__(self):
+        pass
+
     def operation_b(self):
         return "ProductB1 operation"
 
 
 class ProductB2(AbstractProductB):
+    def __init__(self):
+        pass
+
     def operation_b(self):
         return "ProductB2 operation"
 
 
 class AbstractFactory:
+    def __init__(self):
+        raise NotImplementedError("This method should be implemented by subclasses")
+
     def create_product_a(self):
-        pass
+        raise NotImplementedError("This method should be implemented by subclasses")
 
     def create_product_b(self):
-        pass
+        raise NotImplementedError("This method should be implemented by subclasses")
 
 
 class ConcreteFactory1(AbstractFactory):
+    def __init__(self):
+        pass
+
     def create_product_a(self):
         return ProductA1()
 
@@ -45,6 +69,9 @@ class ConcreteFactory1(AbstractFactory):
 
 
 class ConcreteFactory2(AbstractFactory):
+    def __init__(self):
+        pass
+
     def create_product_a(self):
         return ProductA2()
 
