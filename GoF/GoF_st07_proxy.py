e9e7cf4347ee3f581da6edbfa9b4003bb5043200
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-06-05 06:46:53 +0900

    GoF - Proxy

--- GoF/GoF_st07_proxy.py
+++ GoF/GoF_st07_proxy.py
@@ -1,9 +1,15 @@
 class Subject:
+    def __init__(self):
+        raise NotImplementedError("This method should be implemented by subclasses")
+
     def request(self):
         raise NotImplementedError("This method should be implemented by subclasses")
 
 
 class RealSubject(Subject):
+    def __init__(self):
+        pass
+
     def request(self):
         print("RealSubject: Handling request.")
 
