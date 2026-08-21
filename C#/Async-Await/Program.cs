4894054c52c76fb5db9d5fa9e38a9ae51622c9aa
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-08-22 06:26:24 +0900

    C# Async-Await - TryAsyncAwait

--- C#/Async-Await/Program.cs
+++ C#/Async-Await/Program.cs
@@ -7,8 +7,11 @@
 /// </summary>
 void TryTask()
 {
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Starting task...");
+
     Task<int> calculationTask = Task.Run(() =>
     {
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Calculating sum of numbers from 1 to 5...");
         int total = 0;
         for (int i = 1; i <= 5; i++)
         {
@@ -18,12 +21,54 @@ void TryTask()
         return total;
     });
 
-    Console.WriteLine("Working...");
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Working...");
 
-    // Wait for the task to complete and get the result
+    // Wait for the task thread to complete and get the result.
     int result = calculationTask.Result;
-    
-    Console.WriteLine($"Result: {result}");
+
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Result: {result}");
 }
 
+async Task TryAsyncAwait()
+{
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Starting task...");
+
+    Task<int> calculationTask = Task.Run(() =>
+    {
+        Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Calculating sum of numbers from 1 to 5...");
+        int total = 0;
+        for (int i = 1; i <= 5; i++)
+        {
+            total += i;
+            Thread.Sleep(500);
+        }
+
+        return total;
+    });
+
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Working...");
+
+    // Await the task thread to complete and get the result.
+    int result = await calculationTask;
+
+    Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId}: Result: {result}");
+}
+
+// Calls the TryTask method to demonstrate task execution.
+// Console output:
+// Thread 2: Starting task...
+// Thread 2: Working...
+// Thread 4: Calculating sum of numbers from 1 to 5...
+// Thread 2: Result: 15
+Console.WriteLine("=== TryTask ===");
 TryTask();
+
+// Calls the TryAsyncAwait method to demonstrate async/await execution.
+// Console output:
+// Thread 2: Starting task...
+// Thread 2: Working...
+// Thread 4: Calculating sum of numbers from 1 to 5...
+// Thread 4: Result: 15
+Console.WriteLine("=== TryAsyncAwait ===");
+await TryAsyncAwait();
+
