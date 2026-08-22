806196ba0639b498fc76b15ef357554359bd45fe
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-08-23 06:02:35 +0900

    Null-Conditional Assignment

---
+++ C#/NullConditionalAssignment/Program.cs
@@ -0,0 +1,10 @@
+﻿// Check if a string is null and assign a default value using an if statement
+string? str1 = null; // Nullable reference type
+if (str1 == null)
+{
+    str1 = "default value";
+}
+
+// Check if a string is null and assign a default value using the null-coalescing operator (??=)
+string? str2 = null;
+str2 ??= "default value";
