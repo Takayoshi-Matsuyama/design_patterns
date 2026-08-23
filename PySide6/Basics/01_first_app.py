e334363fd081d2ac7bae5ac4d02c7b9e392c72a2
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-08-23 15:09:45 +0900

    PySide - 01_first_app

--- PySide6/Basics/01_first_app.py
+++ PySide6/Basics/01_first_app.py
@@ -2,9 +2,12 @@ from PySide6.QtWidgets import QApplication, QWidget
 
 import sys
 
+# Create the application instance
 app = QApplication(sys.argv)
 
+# Create a window instance and execute show() method to display the window
 window = QWidget()
 window.show()
 
+# Start the application's event loop
 app.exec()
