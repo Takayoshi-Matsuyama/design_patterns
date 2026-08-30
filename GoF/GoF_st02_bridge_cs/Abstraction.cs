5db378ddaec1a2b2c7197cafa513da332f2c06f2
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-08-30 17:49:55 +0900

    GoF - Bridge C#

---
+++ GoF/GoF_st02_bridge_cs/Abstraction.cs
@@ -0,0 +1,35 @@
+// Copyright 2026 Takayoshi Matsuyama
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Filescope namespace (C# 10.0 or later)
+namespace GoF.Bridge;
+
+/// <summary>
+/// Represents the abstraction in the Bridge pattern.
+/// </summary>
+public abstract class Abstraction(IImplementor implementor)
+{
+    /// <summary>
+    /// Gets the implementor associated with this abstraction.
+    /// </summary>
+    /// <value>The implementor instance.</value>
+    /// </summary>
+    protected IImplementor Implementor { get; init; } = implementor;
+
+    /// <summary>
+    /// Executes the operation defined by the abstraction.
+    /// </summary>
+    /// <returns>A string result of the operation.</returns>
+    public virtual string Operation() => Implementor.ExecuteOperation();
+}
