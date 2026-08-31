ae96fa9b4a4b354319666870d5f4c19c932208d6
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-08-31 19:14:58 +0900

    GoF - Prototype Py

--- GoF/GoF_gen04_prototype.py
+++ GoF/GoF_gen04_prototype.py
@@ -16,8 +16,8 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import copy
 from abc import ABC, abstractmethod
+import copy
 from typing import Self
 
 
