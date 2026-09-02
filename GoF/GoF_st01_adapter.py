52615f6287493ddf15bc199872c92d78978a9117
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-09-02 20:39:47 +0900

    GoF - Adapter Py

--- GoF/GoF_st01_adapter.py
+++ GoF/GoF_st01_adapter.py
@@ -1,16 +1,20 @@
-# Copyright 2026 Takayoshi Matsuyama
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""
+Implementation of the Adapter design pattern.
+
+Copyright 2026 Takayoshi Matsuyama
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 from abc import ABC, abstractmethod
 
@@ -20,8 +24,12 @@ class Target(ABC):
 
     @abstractmethod
     def request(self) -> str:
-        """Requests the target operation."""
-        pass  # Should be implemented by concrete subclasses.
+        """Requests the target operation.
+
+        Returns:
+            str: The result of the target request.
+        """
+        ...  # Should be implemented by concrete subclasses.
 
 
 class Adaptee:
