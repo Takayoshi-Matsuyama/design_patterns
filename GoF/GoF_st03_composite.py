69c3475ae7a3eb8a152a60c32d648eec2d52843b
Takayoshi Matsuyama <tkys0515@gmail.com>
2026-09-04 05:57:15 +0900

    GoF - Composite Py

--- GoF/GoF_st03_composite.py
+++ GoF/GoF_st03_composite.py
@@ -1,55 +1,121 @@
-# Copyright 2026 Takayoshi Matsuyama
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+"""
+Implementation of the Composite design pattern.
+
+Copyright 2026 Takayoshi Matsuyama
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
 
 from abc import ABC, abstractmethod
+from typing import Self
+
+
+class NotSupportedError(Exception):
+    """Exception raised when an unsupported operation is attempted."""
+
+    ...  # No additional implementation needed for this exception class.
 
 
 class Component(ABC):
+    """Represents the component interface in the Composite design pattern."""
 
     @abstractmethod
-    def operation(self):
-        pass
+    def operation(self) -> str:
+        """Performs an operation on the component.
+
+        Returns:
+            str: The result of the operation on the component.
+        """
+        ...  # Should be implemented by concrete subclasses.
 
     @abstractmethod
-    def add(self, component):
-        pass
+    def add(self, component: Self) -> None:
+        """Adds a child component to the composite.
+
+        Args:
+            component (Self): The child component to add.
+        """
+        ...  # Should be implemented by concrete subclasses.
 
     @abstractmethod
-    def remove(self, component):
-        pass
+    def remove(self, component: Self) -> None:
+        """Removes a child component from the composite.
+
+        Args:
+            component (Self): The child component to remove.
+        """
+        ...  # Should be implemented by concrete subclasses.
 
     @abstractmethod
-    def get_child(self, index):
-        pass
+    def get_child(self, index: int) -> Self:
+        """Retrieves a child component by its index.
+
+        Args:
+            index (int): The index of the child component to retrieve.
+
+        Returns:
+            Self: The child component at the specified index.
+        """
+        ...  # Should be implemented by concrete subclasses.
 
 
 class Leaf(Component):
+    """Represents a leaf node in the Composite design pattern."""
 
-    def operation(self):
+    def operation(self) -> str:
+        """Performs an operation on the leaf component.
+
+        Returns:
+            str: The result of the operation on the leaf component.
+        """
         return "Leaf operation"
 
-    def add(self, component):
-        raise NotImplementedError("Leaf nodes cannot add components")
+    def add(self, component: Component) -> None:
+        """Attempts to add a child component to the leaf node.
 
-    def remove(self, component):
-        raise NotImplementedError("Leaf nodes cannot remove components")
+        Args:
+            component (Component): The child component to add.
 
-    def get_child(self, index):
-        raise NotImplementedError("Leaf nodes do not have children")
+        Raises:
+            NotSupportedError: Leaf nodes cannot add components.
+        """
+        raise NotSupportedError("Leaf nodes cannot add components")
+
+    def remove(self, component: Component) -> None:
+        """Attempts to remove a child component from the leaf node.
+
+        Args:
+            component (Component): The child component to remove.
+
+        Raises:
+            NotSupportedError: Leaf nodes cannot remove components.
+        """
+        raise NotSupportedError("Leaf nodes cannot remove components")
+
+    def get_child(self, index: int) -> None:
+        """Retrieves a child component by its index.
+
+        Args:
+            index (int): The index of the child component to retrieve.
+
+        Returns:
+            None: Always returns None. Because leaf does not have children.
+        """
+        return None
 
 
 class Composite(Component):
+    """Represents a composite node in the Composite design pattern."""
 
     def __init__(self):
         self.children = []
@@ -70,8 +136,8 @@ class Composite(Component):
         return self.children[index]
 
 
-if __name__ == "__main__":
-
+def main() -> None:
+    """Demonstrates the Composite design pattern."""
     leaf1 = Leaf()
     leaf2 = Leaf()
     composite = Composite()
@@ -79,3 +145,7 @@ if __name__ == "__main__":
     composite.add(leaf2)
 
     print(composite.operation())
+
+
+if __name__ == "__main__":
+    main()
